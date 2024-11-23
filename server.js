819002ce676f392a513727db3198ea54079379ec
Wagner Soares <Wagner.soares@somagrupo.com.br>
2024-11-23 00:32:16 -0300

    deploy do projeto

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+import routes from "./src/routes/postsRoutes.js";
+
+
+
+const app = express();
+app.use(express.static("uploads"));
+routes(app);
+
+
+app.listen(3000, () => {
+    console.log("servidor escutando...");
+});
---
+++ src/routes/postsRoutes.js
@@ -0,0 +1,33 @@
+import express from "express"
+import multer from "multer";
+import cors from "cors";
+import { listarPosts, postarNovoPost, uploadImagem, atualiarNovoPost } from "../controllers/postsControllers.js";
+
+const corsOptions ={
+  origin: "http://localhost:8000",
+  opitionsSuccessStatus: 200
+}
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  const upload = multer({ dest: "./uploads", storage});
+
+
+ const routes = (app) =>{
+    app.use(express.json());
+    app.use(cors(corsOptions));
+    app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost)
+    app.post("/upload", upload.single("imagem"), uploadImagem)
+    app.put("/upload/:id", atualiarNovoPost)
+};
+
+export default routes;
+
